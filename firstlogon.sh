bba28583386f044b6b98f1bcb4cf7db86edf9061
Daniell Mesquita <daniellmesquita@protonmail.com>
2021-11-02 04:07:17 -0400

    detect username and store at /1/config/user.json

--- firstlogon.sh
+++ firstlogon.sh
@@ -15,23 +15,28 @@ cat << "EOF"
 
                   Welcome to Floflis!
 EOF
+
+flouser=$(logname) # considers as main user, the first logged user after first boot 💃
+
 # Core>
    if [ -e /usr/lib/floflis/layers/dna/floflis ]
    then
       if [ -e /usr/lib/floflis/layers/core ]
       then
          echo "Getting and storing username..."
-         #flouser=$(echo ~ | awk -F/ '{print $NF}')
-         cd /home
-         for D in `find . -mindepth 1 -maxdepth 1 -type d`
-         do
-            cd ${D}
-            flouser=${D//./}
-done
-         cat > /1/config/dat.json << ENDOFFILE
-       {"type":"config/os","url":{},"lang":"en-us","title":"Floflis Settings - ${flouser}","user":"${flouser}"}
+#         flouser=$(echo ~ | awk -F/ '{print $NF}')
+#         cd /home
+#         for D in `find . -mindepth 1 -maxdepth 1 -type d`
+#         do
+#            cd ${D}
+#            flouser=${D//./}
+#done
+         cat > /1/config/user.json << ENDOFFILE
+       {"name":""}
 ENDOFFILE
-         sudo sed -i "s/user="blank"/user="${flouser}"/g" /usr/lib/floflis/config
+         contents="$(jq ".name = \"$flouser\"" /1/config/user.json)" && \
+         echo "${contents}" > /1/config/user.json
+#         sudo sed -i "s/user="blank"/user="${flouser}"/g" /usr/lib/floflis/config
 fi
 fi
 # <Core
@@ -40,7 +45,7 @@ fi
 
 if [ ! -e /usr/lib/floflis/layers/server ]
 then
-   echo "Don't install SSH if don't know how bad systemd is or if you think BIOS is a fossil fuel. And never use SSH to remotely access your devices in a public network, such as mobile. SSH can be useful for securer (than HTTP) downloads. Estimated 1MB to download/6 MB installed."
+   echo "Don't install SSH if you think BIOS is a fossil fuel. And never use SSH to remotely access your devices in a public network, such as mobile. SSH can be useful for securer (than HTTP) downloads. Estimated 1MB to download/6 MB installed."
    echo "Do you want to install SSH? [Y/n]"
    read instssh
    case $instssh in
@@ -70,8 +75,20 @@ sudo chown ${flouser}:${flouser} /home/${flouser}/.config/autostart/IPFS.sh
 mv /home/${flouser}/.config/autostart/IPFS.sh /home/${flouser}/.config/autostart/IPFS.desktop
 sudo chown ${flouser}:${flouser} /home/${flouser}/.config/autostart/IPFS.desktop
 
+echo "- Initializing IPFS..."
+ipfs init
+echo "- To start IPFS service, open a new term window and type 'ipfs daemon', or restart your computer after Floflis is fully installed."
+
 echo "- Cleanning install..."
 sudo rm -rf /home/firstlogon.sh
 sudo rm -rf ${flouser}/.config/autostart/firstlogon.desktop
 sudo chmod -R a+rwX /home/${flouser} && sudo chown ${flouser}:${flouser} /home/${flouser}
+
+cd /home
+for D in `find . -mindepth 1 -maxdepth 1 -type d`
+do
+   pure=$(echo "${D}" | tr -d "/" | tr -d ".")
+   rm -f /home/$pure/.config/autostart/firstlogon.desktop
+done
+
 echo "(✓) Floflis has been successfully installed! You can now use it :)"
--- install.sh
+++ install.sh
@@ -299,13 +299,13 @@ fi
    $maysudo systemctl enable flo-init
    $maysudo systemd enable flo-init
    
-   #echo "- Installing Floflis' first boot script..."
-   #$maysudo cp -f /usr/lib/floflis/layers/core/firstboot /etc/init.d && $maysudo rm -f /usr/lib/floflis/layers/core/firstboot
-   #$maysudo chmod +x /etc/init.d/firstboot
-   #$maysudo update-rc.d firstboot defaults
-   #$maysudo update-rc.d firstboot enable
-   #$maysudo systemctl enable firstboot
-   #$maysudo systemd enable firstboot
+   echo "- Installing Floflis' first boot script..."
+   $maysudo cp -f /usr/lib/floflis/layers/core/firstboot /etc/init.d && $maysudo rm -f /usr/lib/floflis/layers/core/firstboot
+   $maysudo chmod +x /etc/init.d/firstboot
+   $maysudo update-rc.d firstboot defaults
+   $maysudo update-rc.d firstboot enable
+   $maysudo systemctl enable firstboot
+   $maysudo systemd enable firstboot
 
    echo "- Installing Floflis Central..."
    $maysudo mv /usr/lib/floflis/layers/core/floflis-central /usr/bin
